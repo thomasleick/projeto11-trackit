32d2bcc252f42c1d07a4107b1a755f00deb2723d
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-14 08:30:03 -0300

    Routes on App component importing each Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from "styled-components"
+import HabitsPage from './pages/HabitsPage/HabitsPage';
+import HistoryPage from './pages/HistoryPage/HistoryPage';
+import HomePage from './pages/HomePage/HomePage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import TodayPage from './pages/TodayPage/TodayPage';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<></>} />
-          <Route path="/cadastro" element={<></>} />
-          <Route path="/habitos" element={<></>} />
-          <Route path="/hoje" element={<></>} />
-          <Route path="/historico" element={<></>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
+          <Route path="/habitos" element={<HabitsPage />} />
+          <Route path="/hoje" element={<TodayPage />} />
+          <Route path="/historico" element={<HistoryPage />} />
         </Routes>
       </Router>
     </>
