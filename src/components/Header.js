67b440546f0e980cc5aef827ab7990398cd63098
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-14 09:30:27 -0300

    fonts included, Header Component 100% and logo

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../images/logo.svg'
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <img src={logo} />
+            <h1>TrackIt</h1>
+        </HeaderContainer>
+    );
+};
+
+const HeaderContainer = styled.header`
+    height: 248px;
+    font-family: 'Playball', cursive;
+
+    img {
+        margin-top: 68px; 
+        margin-left: calc(50% - 78px);
+        width: 156px;
+        height: 93px;
+    }
+
+    h1 {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 68.982px;
+        line-height: 86px;
+        text-align: center;
+        color: #126BA5;
+    }
+`
+
+export default Header;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header';
+
 
 const HomePage = () => {
     return (
-        <div>
+        <>
+        <Header />
             
-        </div>
+            
+        </>
     );
 };
 
