0b5cc18794b51c61356f76cddd85328f0b069c35
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-14 10:15:18 -0300

    persistLogin

---
+++ src/components/PersistLogin.js
@@ -0,0 +1,51 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+import useLocalStorage from "../hooks/useLocalStorage";
+import React from 'react';
+
+const PersistLogin = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const refresh = useRefreshToken()
+    const { auth } = useAuth()
+    const [persist] = useLocalStorage('persist', false)
+    
+    useEffect(() => {
+        let isMounted = true;
+        const verifyRefreshToken = async () => {
+            try {
+                await refresh()
+            } 
+            catch (err) {
+                console.log(err)
+            }
+            finally {
+                isMounted && setIsLoading(false)
+            }
+        }
+
+        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false)
+
+        return () => isMounted = false
+    }, [])
+
+
+/*     useEffect(() => {
+        console.log(`isLoading: ${isLoading}`)
+        console.log(`aT: ${JSON.stringify(auth?.accessToken)}`)
+    }, [isLoading]) */
+
+    return (
+        <>
+            {!persist
+                ? <Outlet />
+            :   isLoading
+                    ?<p>Loading...</p>
+                    :<Outlet />
+            }
+        </>
+    )
+}
+
+export default PersistLogin
