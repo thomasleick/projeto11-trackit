236b81904c4ae9ad41c06991d4691e28e6a2a528
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-15 20:43:51 -0300

    TodaysPage check/uncheck working

---
+++ src/components/TodayHabit.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import useAuth from '../hooks/useAuth';
+import axios from '../api/axios';
+import check1 from '../images/check1.svg';
+import check2 from '../images/check2.svg';
+
+const checks = [check1, check2]
+const TodayHabit = ( props ) => {
+    const { habit, getTodayHabits } = props
+    const [isChecked, setIsChecked] = useState(habit.done)
+    const [isLoading, setIsLoading] = useState(false)
+    const [fetchError, setFetchError] = useState("")
+    const { auth } = useAuth()
+  
+    const handleClick = async (e) => {
+        e.preventDefault()
+        const config = {
+            headers: { Authorization: `Bearer ${auth.accessToken}` }
+        };
+        setIsLoading(true);
+        const STATS_URL = isChecked ? `/habits/${habit.id}/uncheck` : `/habits/${habit.id}/check`
+        try {
+            const response = await axios.post(STATS_URL, {}, config);
+            setFetchError(null);
+            //setIsChecked(!isChecked)
+            fetchHabits();
+        } catch (err) {
+            console.error(err);
+            setFetchError(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const fetchHabits = async () => {
+        setIsLoading(true);
+        try {
+            await getTodayHabits();
+        } catch (err) {
+            setFetchError(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+        };
+
+    return (
+        <HabitConteiner>
+            <h1>{habit.name}</h1>
+            <p>Sequência atual: {`${habit.currentSequence}`} {`${habit.currentSequence > 1 ? "dias" : "dia"}`}<br />
+            Seu record: {`${habit.highestSequence}`} {`${habit.highestSequence > 1 ? "dias" : "dia"}`}</p>
+            <img src={checks[isChecked ? 1 : 0]} alt="Concluído" onClick={isLoading ? undefined : e => handleClick(e)} />
+        </HabitConteiner>
+    );
+};
+
+const HabitConteiner = styled.div`
+    position: relative;
+    width: 310px;
+    height: 64px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    margin: 10px 0;
+    padding: 15px;
+
+    h1 {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        color: #666666;
+    }
+    p {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12.976px;
+        line-height: 16px;
+        color: #666666;
+        margin-top: 7px;
+    }
+    img {
+        position: absolute;
+        top: calc(50% - 34.5px);
+        right: 12px;
+    }
+`
+
+export default TodayHabit;
--- src/pages/TodayPage/TodayPage.js
+++ src/pages/TodayPage/TodayPage.js
@@ -1,14 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import TodayHabit from '../../components/TodayHabit';
 import styled from 'styled-components';
+import axios from '../../api/axios';
+import useAuth from '../../hooks/useAuth';
+import { Comment } from 'react-loader-spinner';
+
+const TODAY_URL = "/habits/today"
 
 const TodayPage = () => {
+
+    const { auth } = useAuth()
+    const [isLoading, setIsLoading] = useState(false)
+    const [fetchError, setFetchError] = useState("")
+    const [todayHabits, setTodayHabits] = useState("")
+
+    const date = new Date()
+
+    function getDayOfWeek(date) {
+        const dayOfWeek = new Date(date).getDay();    
+        return isNaN(dayOfWeek) ? null : 
+          ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'][dayOfWeek];
+      }
+
+    const weekday = getDayOfWeek(date)
+    let isMounted = true;
+    const controller = new AbortController();
+    const config = {
+      headers: { Authorization: `Bearer ${auth.accessToken}` },
+    };
+    const getTodayHabits = async () => {
+        setIsLoading(true);
+        try {
+          const response = await axios.get(TODAY_URL, config, {
+            signal: controller.signal,
+          });
+          setTodayHabits(response.data);
+          setFetchError(null);
+        } catch (err) {
+          console.error(err);
+          setFetchError(err.message);
+          setTodayHabits([]);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+    
+      useEffect(() => {
+
+        getTodayHabits();
+
+        return () => {
+          isMounted = false;
+          controller.abort();
+        };
+      }, []);
+
     return (
         <>
             <Header signedIn={true} />
             <Main>
-             
+                <h1>{weekday}, {("0" + date.getDate()).slice(-2)}/{("0" + (date.getMonth() + 1)).slice(-2)}</h1>
+                <p>{"Nenhum hábito concluído ainda"}</p>
+                {isLoading && !fetchError && (
+                    <StatusMsg>
+                        <Comment
+                            visible={true}
+                            height="80"
+                            width="80"
+                            ariaLabel="comment-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="comment-wrapper"
+                            color="#fff"
+                            backgroundColor="#52B6FF"
+                        />
+                    </StatusMsg>
+      )}
+                {!isLoading && !fetchError && ( todayHabits?.length ?
+                    todayHabits.map((habit, id) => <TodayHabit key={id} habit={habit} getTodayHabits={getTodayHabits}/>) 
+                :
+                    <p>Teste</p> )
+                }
             </Main>
             <Footer />
         </>
@@ -16,6 +89,41 @@ const TodayPage = () => {
 };
 const Main = styled.main`
     background: #E5E5E5;
-    height: calc(100vh - 140px);
+    height: calc(100vh - 198px);
+    padding: 29px 17px;
+
+    h1 {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22.976px;
+        line-height: 29px;
+        color: #126BA5; 
+    }
+
+    p {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17.976px;
+        line-height: 22px;
+        color: #BABABA;
+        margin-bottom: 23px;
+    }
 `
+const StatusMsg = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17.976px;
+  line-height: 22px;
+  color: #666666;
+  padding: 28px 17px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-weight: 700;
+  }
+`;
 export default TodayPage;
